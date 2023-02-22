7a328719ce962e6b487604b1559232ad298c48dc
aleee-git <alejandraramirez.niu@nehemiahiu.com>
2023-02-22 17:48:41 -0600

    Conexion a DB

---
+++ includes/database.php
@@ -0,0 +1,14 @@
+<?php 
+
+// Conexion de base de datos
+$db = mysqli_connect('localhost', 'root', 'Blueberry8!', 'salonapp');
+
+// Comprobar que la base exista/este llena y confirmar conexion 
+if (!$db) {
+    echo 'Connection Failed :(';
+    exit;
+} 
+
+echo 'Successful Connection :)';
+
+?>
